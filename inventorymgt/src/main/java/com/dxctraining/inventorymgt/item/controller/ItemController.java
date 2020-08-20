48dc1c0c045193dd6b09fb3ba075adf1a96e608e
fazilshaik <shaikfazil1.sf@gmail.com>
2020-08-21 02:50:09 +0530

    sprint-11 completed

--- inventorymgt/src/main/java/com/dxctraining/inventorymgt/item/controller/ItemController.java
+++ inventorymgt/src/main/java/com/dxctraining/inventorymgt/item/controller/ItemController.java
@@ -7,9 +7,12 @@ import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.dxctraining.inventorymgt.item.dto.CreateComputerRequest;
 import com.dxctraining.inventorymgt.item.entities.Computer;
+import com.dxctraining.inventorymgt.item.entities.Item;
 import com.dxctraining.inventorymgt.item.entities.Phone;
 import com.dxctraining.inventorymgt.item.service.IItemService;
 import com.dxctraining.inventorymgt.supplier.entities.Supplier;
@@ -52,5 +55,28 @@ public class ItemController {
 		ModelAndView modelAndView = new ModelAndView("phonelist","phones", phone);
 		return modelAndView;
 	}
+	
+	@GetMapping("/addcomputer")
+	public ModelAndView addComputer() {
+		ModelAndView modelAndView = new ModelAndView("addcomputer");
+		return modelAndView;
+	}
+	
+	@GetMapping("/processaddcomputer")
+	public ModelAndView processAddComputer(@RequestParam("name")String name, @RequestParam("discsize")int discsize) {
+		Computer computer = new Computer(name, discsize);
+		Item item = (Item)computer;
+		item = service1.addItem(item);
+		ModelAndView modelAndView = new ModelAndView("processaddcomputer","computer",item);
+		return modelAndView;
+	}
+	
+	@GetMapping("/postaddcomputer")
+	public ModelAndView postAddComputer() {
+		CreateComputerRequest computer = new CreateComputerRequest();
+		ModelAndView modelAndView = new ModelAndView("postaddcomputer","computer",computer);
+		return modelAndView;
+	}
+	
 
 }
